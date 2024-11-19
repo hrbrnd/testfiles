b61f661b1dfa56af6a4e2cbbf6eae182ad3676c2
hrbrnd <you@example.com>
2024-11-19 13:33:08 -0500

    added missing references

--- scripts/list_all_instances.py
+++ scripts/list_all_instances.py
@@ -126,10 +126,66 @@ def write_to_csv(file_path, header, rows):
         writer.writerow(header)  # Write the header row
         writer.writerows(rows)  # Write the data rows
 
+def get_instance_tags(aws_profile, regions):
+    """
+    Collect all unique tag keys across all EC2 instances in the provided regions.
+    
+    :param aws_con: The boto3 session object.
+    :param regions: List of region names to query.
+    :return: Sorted list of unique tag keys.
+    """
+    # Prepare to collect all unique tag keys across all instances
+    tag_keys = set()
+
+    # First pass to collect unique tag keys
+    for region in regions:
+        ec2_client = get_credentials(aws_profile, region)
+        paginator = ec2_client.get_paginator("describe_instances")
+
+        for page in paginator.paginate():
+            for each_item in page["Reservations"]:
+                for instance in each_item["Instances"]:
+                    for each_tag in instance.get("Tags", []):
+                        tag_keys.add(each_tag['Key'])
+    tag_keys = sorted(tag_keys)
+    return tag_keys        
+
+
+def calculate_total_ebs_size(ec2_client, instance_id):
+    """
+    Calculate the total sum of all EBS volumes attached to an EC2 instance.
+    
+    :param instance_id: The ID of the EC2 instance.
+    :param region: The AWS region where the instance is located.
+    :return: The total size of all attached EBS volumes in GiB.
+    """
+
+    # Get the list of volumes attached to the EC2 instance
+    response = ec2_client.describe_instances(InstanceIds=[instance_id])
+
+    # Initialize total size variable
+    total_size = 0
+
+    # Iterate over each instance to get the attached volumes
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            # Check for the 'BlockDeviceMappings' field (attached volumes)
+            if 'BlockDeviceMappings' in instance:
+                for block_device in instance['BlockDeviceMappings']:
+                    # Get the volume ID from the block device mapping
+                    volume_id = block_device['Ebs']['VolumeId']
+                    
+                    # Get the volume's size
+                    volume_response = ec2_client.describe_volumes(VolumeIds=[volume_id])
+                    for volume in volume_response['Volumes']:
+                        total_size += volume['Size']  # Add the size of this volume
+
+    return total_size
+
 def main():
     #set aws profile
-    aws_profile = ""
-    base_output_filename = ""
+    aws_profile = "hbm"
+    base_output_filename = "D:\\HBM\\Documents\\Personal\\CertsLearning\\boto3-AWS-oreilly\\pythonProject\\Output\\instance_tags_with_details.csv"
 
     ec2_client = get_credentials(aws_profile)
     regions  = get_all_regions(ec2_client)
