89ac4fa0ff5993c6ed7ebb25b726a39affae6118
hrbrnd <you@example.com>
2024-11-26 16:08:03 -0500

    Added methods

--- methods/aws_methods.py
+++ methods/aws_methods.py
@@ -11,20 +11,6 @@ def get_credentials(aws_profile, region = "us-east-1", service = "ec2"):
     :return: List of region names (e.g., ['us-east-1', 'us-west-2']).
     """
     aws_con = boto3.session.Session(profile_name = aws_profile, region_name = region)
-    ec2_client = aws_con.client(service)
+    service_client = aws_con.client(service)
 
-    return ec2_client
-
-
-def get_all_regions(ec2_client):
-    """
-    Get the list of all available AWS regions.
-    
-    :param profile_name: The AWS CLI profile name to use.
-    :return: List of region names (e.g., ['us-east-1', 'us-west-2']).
-    """
-    region_response = ec2_client.describe_regions()
-    regions = [region["RegionName"] for region in region_response["Regions"]]
-
-    return regions
-
+    return service_client
