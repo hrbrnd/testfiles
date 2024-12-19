c0071fc6650be6229a1c9feb44cf7c4deb566559
hrbrnd <you@example.com>
2024-12-19 12:39:41 -0500

    baseline serveless methods

--- methods/serverless_methods.py
+++ methods/serverless_methods.py
@@ -1,23 +1,37 @@
 from methods.aws_methods import get_credentials
 
-def get_lambda(aws_profile, regions):
-    """Get all load balancers (Afunction and Nfunction) in the specified region."""
+def get_all_lambda_functions(aws_profile, regions):
+
+
     lambda_details = []
 
     for region in regions:
-        lambda_client = get_credentials(aws_profile, region, service="lambda")
-        paginator = lambda_client.get_paginator("list_functions")
-        for page in paginator.paginate():
+        lambda_client = get_credentials(aws_profile, region, service= "lambda")
+        paginator = lambda_client.get_paginator('list_functions')
+        
+        page_iterator = paginator.paginate()
+        
+        for page in page_iterator:
             for function in page['Functions']:
-                print(function)
-    #             row = [
-    #                 region,
-    #                 function['LoadBalancerName'],
-    #                 function['DNSName'],
-    #                 function['CreatedTime'],
-    #                 function['Type'],
-    #                 function['State']['Code'],
-    #                 function.get('Tags', [])
-    #             ]
-    #             lambda_details.append(row)
-    # return lambda_details    
+                function_arn = function['FunctionArn']
+                function_details = lambda_client.get_function(FunctionName=function_arn)
+                
+                tags = lambda_client.list_tags(Resource=function_arn).get('Tags', {})
+                
+                row = {
+                    region,
+                    function['FunctionName'],
+                    function['Runtime'],
+                    function['Handler'],
+                    function['Role'],
+                    function['CodeSize'],
+                    function.get('Description', 'N/A'),
+                    function['Timeout'],
+                    function['MemorySize'],
+                    function['LastModified'],
+                    tags
+                }
+                
+                lambda_details.append(row)
+    
+    return lambda_details
